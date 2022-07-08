4f1f4436a35991a77af8fb67225a96b7df277e7e
ayushpaine <ayushmt701@gmail.com>
2022-07-09 00:30:01 +0530

    variables - let & const

---
+++ src/vars.rs
@@ -0,0 +1,21 @@
+//variables hold primitive data or references to data
+//are immutable by default
+//block-scoped:rust
+
+pub fn run() {
+    let name = "Ayush";
+    let mut age = 19;
+
+    println!("My name is {} and I am {} years old", name, age);
+
+    age = 18;
+
+    println!("My name is {} and I am {} years old", name, age);
+
+    const ID: i32 = 001; //explicitly state type for constants
+    println!("ID: {}", ID);
+
+    //assign multiple values
+    let (my_name, my_age) = ("Ayush", 19);
+    println!("{} is {}", my_name, my_age);
+}
